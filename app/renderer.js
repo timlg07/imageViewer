08c8e5aafdaf018aeef982c8f3b7eb1e18ecf966
Tim Greller <timlg07@gmail.com>
2020-12-19 22:45:06 +0100

    Prepared dist argument testing.

--- app/renderer.js
+++ app/renderer.js
@@ -1,2 +1,5 @@
+const { remote } = require("electron");
 const canvas = document.getElementById("app-content");
 const context = canvas.getContext("2d");
+const inputArgs = remote.process.argv;
+console.log(inputArgs);
