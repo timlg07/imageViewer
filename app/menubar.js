3751cd288a9d8c281e900c348f703a7ac8d3670d
Tim Greller <timlg07@gmail.com>
2020-12-19 18:22:31 +0100

    Using MenuItem roles rather than defining the items customly.

--- app/menubar.js
+++ app/menubar.js
@@ -3,16 +3,20 @@ const { remote, ipcRenderer } = require("electron");
 module.exports = {
     createMenu() {
         const menu = new remote.Menu();
-        
+
         menu.append(new remote.MenuItem({
             label: 'File',
             submenu: [
-                {
-                    label: 'Exit',
-                    click: () => ipcRenderer.send("quit")
-                }
+                { role: "quit" },
             ]
         }));
+
+        menu.append(new remote.MenuItem({
+            label: 'View',
+            submenu: [
+                { role: 'togglefullscreen' }
+            ]
+        }))
 
         return menu;
     }
