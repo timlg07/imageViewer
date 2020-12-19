799ceed8e227896ed2f9f7f1d5d82388c280cac0
Tim Greller <timlg07@gmail.com>
2020-12-20 00:54:47 +0100

    Implemented basic image viewing capability.

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,21 @@
 const { remote } = require("electron");
+const path = require("path");
+const fs = require("fs")
+
 const canvas = document.getElementById("app-content");
 const context = canvas.getContext("2d");
-const inputArgs = remote.process.argv;
-console.log(inputArgs);
+
+let inputArgs = remote.process.argv;
+const self = inputArgs.shift();
+
+
+function slash(str) {
+    return str.replace(/\\/g, "/");
+}
+
+function encodeChars(str) {
+    return str.replace(/['()# ]/g, c => ('%' + c.charCodeAt(0).toString(16)));
+}
+
+
+document.getElementById("image").style.backgroundImage = "url(" + encodeChars(slash(path.normalize(inputArgs[0]))) + ")";
